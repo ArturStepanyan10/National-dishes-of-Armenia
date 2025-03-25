3ef3d3413e2c2c5ffbb4df4f3889d1cf0d7a923b
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-03-25 14:21:15 +0300

    Initial commit

--- National_dishes_Armenia/settings.py
+++ National_dishes_Armenia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4+=kzx%%8@*vx-)ogmhf!tuq8dsf4ujfg4tfj0q9!m16+_mk(k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
